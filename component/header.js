9db57103c3d45f46f66a585b9d0eec8c12f28608
TheNofis <matbey122@gmail.com>
2024-11-06 18:23:52 +0300

    add: change theme

--- component/header.js
+++ component/header.js
@@ -36,6 +36,35 @@ header.innerHTML = `
       <a class="header__menu-link" href="/page/form">Form</a>
     </li>
   </ul>
+  <div class="theme">
+    <input id="theme-toggle" type="checkbox" />
+    <label for="theme-toggle"></label>
+  </div>
 </nav>
 `;
+
 document.body.appendChild(header);
+
+document.getElementById("theme-toggle").addEventListener("change", (event) => {
+  if (event.target.checked) {
+    document.documentElement.style.setProperty("--background", "#ecf2f8f0");
+    document.documentElement.style.setProperty("--black", "#89929b");
+    document.documentElement.style.setProperty("--red", "#fa7970");
+    document.documentElement.style.setProperty("--green", "#7ce38b");
+    document.documentElement.style.setProperty("--yellow", "#faa356");
+    document.documentElement.style.setProperty("--blue1", "#77bdfb");
+    document.documentElement.style.setProperty("--purple", "#cea5fb");
+    document.documentElement.style.setProperty("--blue2", "#a2d2fb");
+    document.documentElement.style.setProperty("--white", "#161b22");
+  } else {
+    document.documentElement.style.setProperty("--background", "#272a34f0");
+    document.documentElement.style.setProperty("--black", "#282c34");
+    document.documentElement.style.setProperty("--red", "#e06c75");
+    document.documentElement.style.setProperty("--green", "#98c379");
+    document.documentElement.style.setProperty("--yellow", "#e5c07b");
+    document.documentElement.style.setProperty("--blue1", "#61afef");
+    document.documentElement.style.setProperty("--purple", "#c678dd");
+    document.documentElement.style.setProperty("--blue2", "#56b6c2");
+    document.documentElement.style.setProperty("--white", "#abb2bf");
+  }
+});
