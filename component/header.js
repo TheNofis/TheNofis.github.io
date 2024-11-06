8ff30f3659fb833e71d7901347b1343930803ef6
TheNofis <matbey122@gmail.com>
2024-11-06 18:54:02 +0300

    red: add validate function

--- component/header.js
+++ component/header.js
@@ -9,33 +9,33 @@ header.innerHTML = `
     <li class=${locations === "/" || locations === "/index" ? "header__menu-item--active" : "header__menu-item"}>
       <a class="header__menu-link" href="/">Главная</a>
     </li>
-    <li class=${locations === "/page/verstka" ? "header__menu-item--active" : "header__menu-item"}>
+    <li class=${validate(locations === "/page/verstka")}>
       <a class="header__menu-link" href="/page/verstka">Verstka</a>
     </li>
-    <li class=${locations === "/page/grid" ? "header__menu-item--active" : "header__menu-item"}>
+    <li class=${validate(locations === "/page/grid")}>
       <a class="header__meni-link" href="/page/grid">Grid</a>
     </li>
-    <li class=${locations === "/page/modal" ? "header__menu-item--active" : "header__menu-item"}>
+    <li class=${validate(locations === "/page/modal")}>
       <a class="header__menu-link" href="/page/modal">Modal</a>
     </li>
-    <li class=${locations === "/page/grid-layout" ? "header__menu-item--active" : "header__menu-item"}>
+    <li class=${validate(locations === "/page/grid-layout")}>
       <a class="header__menu-link" href="/page/grid-layout"
         >Grid layout</a
       >
     </li>
-    <li class=${locations === "/page/iframe" ? "header__menu-item--active" : "header__menu-item"}>
+    <li class=${validate(locations === "/page/iframe")}>
       <a class="header__menu-link" href="/page/iframe">Iframe</a>
     </li>
-    <li class=${locations === "/page/flow" ? "header__menu-item--active" : "header__menu-item"}>
+    <li class=${validate(locations === "/page/flow")}>
       <a class="header__menu-link" href="/page/flow">Flow</a>
     </li>
-    <li class=${locations === "/page/pseudo" ? "header__menu-item--active" : "header__menu-item"}>
+    <li class=${validate(locations === "/page/pseudo")}>
       <a class="header__menu-link" href="/page/pseudo">Pseudo</a>
     </li>
-    <li class=${locations === "/page/form" ? "header__menu-item--active" : "header__menu-item"}>
+    <li class=${validate(locations === "/page/form")}>
       <a class="header__menu-link" href="/page/form">Form</a>
     </li>
-    <li class=${locations === "/page/variables" ? "header__menu-item--active" : "header__menu-item"}>
+    <li class=${validate(locations, "/page/variables")}>
       <a class="header__menu-link" href="/page/variables">Variables</a>
     </li>
   </ul>
@@ -47,6 +47,11 @@ header.innerHTML = `
 `;
 
 document.body.appendChild(header);
+
+function validate(location, pageLocation) {
+  if (location === pageLocation) return "header__menu-item--active";
+  else return "header__menu-item";
+}
 
 document.getElementById("theme-toggle").addEventListener("change", (event) => {
   if (event.target.checked) {
