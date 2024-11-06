d285402b8da0dd59633ccac0101fba9dc8a1f553
TheNofis <matbey122@gmail.com>
2024-11-06 20:38:04 +0300

    add: variables

--- component/header.js
+++ component/header.js
@@ -84,25 +84,25 @@ document.getElementById("theme-toggle").addEventListener("change", (event) => {
 
 document.getElementById("toggle-header").addEventListener("click", () => {
   const header = document.querySelector(".header");
+  const toggleHeader = document.querySelector("#toggle-header");
+  const content = document.querySelector(".content");
   if (header.style.transform === "translateY(-100%)") {
     header.style.transform = "translateY(0)";
-    document.querySelector(".content").style.transform = "translateY(0)";
-    document.querySelector("#toggle-header").style.top = "0";
-    document.querySelector("#toggle-header").innerHTML = `
+    content.style.transform = "translateY(0)";
+    toggleHeader.style.top = "0";
+    toggleHeader.innerHTML = `
   <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
 `;
   } else {
     header.style.transform = "translateY(-100%)";
-    document.querySelector(".content").style.transform =
-      `translateY(-${document.querySelector(".header").offsetHeight - 50}px)`;
+    content.style.transform = `translateY(-${document.querySelector(".header").offsetHeight - 50}px)`;
   }
 
   setTimeout(() => {
     if (header.style.transform === "translateY(-100%)") {
-      document.querySelector("#toggle-header").style.top = "100%";
-      document.querySelector("#toggle-header").innerHTML =
-        `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/></svg>`;
+      toggleHeader.style.top = "100%";
+      toggleHeader.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/></svg>`;
     }
   }, 500);
 });
